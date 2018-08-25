051cd40a33bca47172fe0b22c1c5287f07998ebe
Paul Marrable <flumpstudios@gmail.com>
2018-08-25 23:02:35 +0100

    added ascending/decending filter options in view

--- RetroCollectNew/wwwroot/js/site.js
+++ RetroCollectNew/wwwroot/js/site.js
@@ -2,27 +2,30 @@
 //TODO: Move into seperate JS file
 
 $(document).ready(function () {
+
+    //Add new game
     $('.add-new-game').click(function (data) {
         var clientListModel = { gameId: this.id };
-        console.log(clientListModel);
-
+        
         $.post("/ClientGamesList/Create", clientListModel, function (data, status) {
-            console.log(data);
-            console.log(status);
             if (status === "success") { alert("Game added to your library") }
             else { alert("There was an error adding game to your library") };
         });
     });
 
 
+    //Sorting
     $('.sorting-headers').click(function (data) {
 
         var sortSwitch = $('#switchsort').val();
-        if (sortSwitch) sortSwitch = false;
+        if (sortSwitch == 'true' || sortSwitch == true) sortSwitch = false;
         else sortSwitch = true;
 
         $('#sortingOptions').val(this.id);
         $('#switchsort').val(sortSwitch);
+
         $('#game-form').submit();
     });
+
+
 });
